b0ecfc32f4c41f2e91e34200e740db955bdf7899
dgketchum <dgketchum@gmail.com>
2025-09-01 12:17:03 -0600

    writing GSHP extracts to tables

--- map/data/ee_tables.py
+++ map/data/ee_tables.py
@@ -5,8 +5,8 @@ import json
 import pandas as pd
 
 
-def concatenate_and_join(ee_in_dir, rosetta_pqt, out_file, index_col='site_id',
-                         categorical_mappings_json=None, categories=None):
+def concatenate_and_join(ee_in_dir, out_file, rosetta_pqt=None, index_col='site_id', categorical_mappings_json=None,
+                         categories=None, dropcols=None):
     """
     Concatenates CSVs from Earth Engine extraction, joins with Rosetta data,
     and saves to a single Parquet file.
@@ -31,18 +31,31 @@ def concatenate_and_join(ee_in_dir, rosetta_pqt, out_file, index_col='site_id',
             continue
 
     ee_df = pd.concat(df_list, ignore_index=True)
-    ee_df = ee_df.drop(columns=['.geo', 'system:index', 'MGRS_TILE'])
+
+    for c in ['.geo', 'system:index', 'MGRS_TILE']:
+        if dropcols is None and c in ee_df.columns:
+            dropcols = [c]
+        elif dropcols and c not in dropcols and c in ee_df.columns:
+            dropcols.append(c)
+        else:
+            pass
+
+    if dropcols is not None:
+        ee_df = ee_df.drop(columns=dropcols)
 
     ee_df.set_index(index_col, inplace=True)
 
-    rosetta_df = pd.read_parquet(rosetta_pqt)
-    rosetta_df = rosetta_df.groupby(index_col).first()
+    if rosetta_pqt:
+        rosetta_df = pd.read_parquet(rosetta_pqt)
+        rosetta_df = rosetta_df.groupby(index_col).first()
 
-    for col in ee_df.columns.to_list():
-        if col in rosetta_df:
-            ee_df = ee_df.drop(columns=[col])
+        for col in ee_df.columns.to_list():
+            if col in rosetta_df:
+                ee_df = ee_df.drop(columns=[col])
 
-    final_df = ee_df.join(rosetta_df, how='left')
+        final_df = ee_df.join(rosetta_df, how='left')
+    else:
+        final_df = ee_df
     final_df = final_df[sorted(final_df.columns.to_list())]
 
     if categorical_mappings_json:
@@ -50,7 +63,8 @@ def concatenate_and_join(ee_in_dir, rosetta_pqt, out_file, index_col='site_id',
         for col in categories:
             if final_df[col].dtype == 'object':
                 final_df[col] = final_df[col].astype('category')
-            mappings[col] = {int(k): int(v) for v, k in enumerate(final_df[col].unique())}
+            mappings[col] = {int(k): int(v) for v, k in
+                             enumerate(final_df[col].unique())}  # likely error if non-numeric categories
 
         with open(categorical_mappings_json, 'w') as f:
             json.dump(mappings, f, indent=4)
@@ -66,11 +80,33 @@ def concatenate_and_join(ee_in_dir, rosetta_pqt, out_file, index_col='site_id',
 
 if __name__ == '__main__':
     """"""
-    run_mt_mesonet_workflow = True
+    run_mt_mesonet_workflow = False
     run_general_workflow = False
+    use_rosetta = False
+    run_gshp_workflow = True
 
     home = os.path.expanduser('~')
     root_ = os.path.join(home, 'data', 'IrrigationGIS')
+
+    if run_gshp_workflow:
+        extracts_dir_ = os.path.join(root_, 'soils', 'swapstress', 'extracts', 'gshp_extracts')
+        output_file_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'gshp_training_data.parquet')
+        mappings_json_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'gshp_categorical_mappings.json')
+
+        concatenate_and_join(ee_in_dir=extracts_dir_, out_file=output_file_, rosetta_pqt=None, index_col='uid',
+                             categorical_mappings_json=mappings_json_,
+                             categories=['hhs_stc',
+                                         'glc10_lc',
+                                         'WRB4',
+                                         'WRB_PHASES',
+                                         'WRB2_CODE',
+                                         'FAO90',
+                                         'KOPPEN',
+                                         'TEXTURE_USDA'],
+                             dropcols=['HWSD2_ID',
+                                       'WISE30s_ID',
+                                       'COVERAGE',
+                                       'SHARE'])
 
     if run_mt_mesonet_workflow:
         extracts_dir_ = os.path.join(root_, 'soils', 'swapstress', 'mt_mesonet_extracts')
@@ -78,12 +114,9 @@ if __name__ == '__main__':
         output_file_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'mt_training_data.parquet')
         mappings_json_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'mt_categorical_mappings.json')
 
-        concatenate_and_join(ee_in_dir=extracts_dir_,
-                             rosetta_pqt=rosetta_file_,
-                             out_file=output_file_,
-                             index_col='station',
-                             categorical_mappings_json=mappings_json_,
-                             categories=['cdl_mode', 'nlcd'])
+        concatenate_and_join(ee_in_dir=extracts_dir_, out_file=output_file_,
+                             rosetta_pqt=rosetta_file_ if use_rosetta else None, index_col='station',
+                             categorical_mappings_json=mappings_json_, categories=['cdl_mode', 'nlcd'])
 
     elif run_general_workflow:
         extracts_dir_ = os.path.join(root_, 'soils', 'swapstress', 'extracts')
@@ -91,10 +124,7 @@ if __name__ == '__main__':
         output_file_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'training_data.parquet')
         mappings_json_ = os.path.join(root_, 'soils', 'swapstress', 'training', 'categorical_mappings.json')
 
-        concatenate_and_join(ee_in_dir=extracts_dir_,
-                             rosetta_pqt=rosetta_file_,
-                             out_file=output_file_,
-                             index_col='site_id',
-                             categorical_mappings_json=mappings_json_,
-                             categories=['cdl_mode', 'nlcd'])
+        concatenate_and_join(ee_in_dir=extracts_dir_, out_file=output_file_,
+                             rosetta_pqt=rosetta_file_ if use_rosetta else None, index_col='site_id',
+                             categorical_mappings_json=mappings_json_, categories=['cdl_mode', 'nlcd'])
 # ========================= EOF ====================================================================
